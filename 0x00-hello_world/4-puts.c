85b1a61dcf3559f2fa6ac24a3358fb12de999cb2
Akeliluhaile <akeliluhaile28@gmail.com>
2022-03-10 11:21:39 -0800

    put file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
 /*
- *
- * main - Prints a text
- *
- *  
- *
+ * main - Prints a 
  * Return: Always 0 (Success)
- *
  */
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle"); 		        return (0);
 
 }
