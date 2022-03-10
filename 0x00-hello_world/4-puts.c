8d221e631ba2378db0467f29e2fbf261fc949634
Akeliluhaile <akeliluhaile28@gmail.com>
2022-03-10 11:36:27 -0800

    puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,4 @@
 #include <stdio.h>
-
 int main(void)
-
-{
-
-	puts("\"Programming is like building a multilingual puzzle"); 		        return (0);
-
+{	puts("Programming is like building a multilingual puzzl\n"); 		        return (0);
 }
