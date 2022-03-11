5c77634ceb5e868698ba5c1d8b25dc631b92e722
Akeliluhaile <akeliluhaile28@gmail.com>
2022-03-11 06:07:47 -0800

    string display

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main-prints string in the put function
+ * Description: using the main function
+ * This program prints  the given string inside double qote
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
