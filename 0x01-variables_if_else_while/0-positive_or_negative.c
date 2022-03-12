f438f409d897cb28d83680483d6a98cf113b3d36
Akeliluhaile <akeliluhaile28@gmail.com>
2022-03-12 10:07:58 -0800

    int type

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - this gives type of int
  * Return: Always sucess
@@ -6,6 +8,9 @@
 int main(void)
 {
 	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
