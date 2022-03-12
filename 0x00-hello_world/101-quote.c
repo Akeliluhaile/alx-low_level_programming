5f3bce7bb26bf35789fdb16b10a724853501b7bb
Akeliluhaile <akeliluhaile28@gmail.com>
2022-03-12 06:05:43 -0800

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - using write method
  * Description: this is to dispay a string
- * Return: 1
+ * Return: Always  1 (success)
  */
-int main(void)
+int  main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 59");
 	return (1);
 }
