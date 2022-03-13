999160335547a8598a6370949976a2233602857f
Akeliluhaile <akeliluhaile28@gmail.com>
2022-03-12 22:38:54 -0800

    alphabet putchr

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints some letters of alphabt
+ * Return: Always (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
