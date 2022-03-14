82e49b018b522f50e36901b67a6b3f9e4d869e51
Akeliluhaile <akeliluhaile28@gmail.com>
2022-03-14 10:54:18 -0700

    combo5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,27 +11,29 @@ int main(void)
 
 	for (a = '0'; a <= '9'; a++)
 	{
-		for (b = '0'; b <= '8'; b++)
+		for (b = '0'; b <= '9'; b++)
 		{
 			for (c = '0'; c <= '9'; c++)
 			{
-				for (d = '1'; d <= '9'; d++)
+				for (d = '0'; d <= '9'; d++)
 				{
-					putchar(a);
-					putchar(b);
-					putchar(' ');
-					putchar(c);
-					putchar(d);
-
-				if (((a == '8') && (b == '9')) && ((c == '9') && (d == '9')))
-				{
-					break;
-				}
-				else
-				{
+					if ('a' != 'b' != 'c' != 'd')
+					{
+						putchar(a);
+						putchar(b);
+						putchar(' ');
+						putchar(c);
+						putchar(d);
+					}
+					if (((a == '8') && (b == '9')) && ((c == '9') && (d == '9')))
+					{
+						break;
+					}
+					else
+					{
 					putchar(',');
 					putchar(' ');
-				}
+					}
 				}
 			}
 		}
