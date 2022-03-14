c54330f7c696e09488f6a471877c32f54d88f7fd
Akeliluhaile <akeliluhaile28@gmail.com>
2022-03-14 07:16:01 -0700

    comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ * main - combinantion of two two digit numbers
+ *
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <= '9'; c++)
+			{
+				for (d = '0'; d <= '9'; d++)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+
+				if (((a == '8') && (b == '9')) && ((c == '9') && (d == '9')))
+				{
+					break;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				}
+			}
+		}
+	}
+
+	puchar('\n');
+
+	return (0);
+}
