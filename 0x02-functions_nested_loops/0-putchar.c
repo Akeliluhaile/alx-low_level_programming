08aaecef6115e52f6fc6d5207530843e546faa55
Akeliluhaile <akeliluhaile28@gmail.com>
2022-03-15 18:21:50 -0700

    display _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
-#ifndef main.h
-#define main.h
+#include <stdio.h>
+#include "main.h"
 /**
  * main - print _putchar
  * Return: Always 0
@@ -7,16 +7,16 @@
 
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
-#endif
+
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef main.h
+#define main.h(_putchar)
+
+int main(void)
+{
+	_putchar = putchar;
+}
+#endif
