28cafbf0b2b010c153276193a60a08f3215e9965
Akeliluhaile <akeliluhaile28@gmail.com>
2022-03-19 11:20:24 -0700

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
  *_abs - gives abslote value of an intiger
- * @int:intiger
+ * @n:intiger
  * Return: Always (success)
  */
-int _abs(int)
+int _abs(int n)
 {
 
 
-	if (int > 0)
+	if (n > 0)
 	{
 		_putchar(int);
 	}
-	else if (int < 0)
+	else if (n < 0)
 	{
 		_putchar(-1 * int);
 	}
