df2f045277138ccb0178a9279d82b99822549422
Akeliluhaile <akeliluhaile28@gmail.com>
2022-03-19 12:26:08 -0700

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,18 +8,14 @@ int _abs(int n)
 {
 
 
-	if (n > 0)
+	if (n >= 0)
 	{
-		_putchar(n);
-	}
-	else if (n < 0)
-	{
-		
-		_putchar(-1 * n);
+		_putchar('n);
 	}
 	else
 	{
-		_putchar(0);
+		
+		_putchar(-1 * n);
 	}
 	return (0);
 
