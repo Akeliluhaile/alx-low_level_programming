19b032c589051ea633073d970af9b93e10264727
Akeliluhaile <akeliluhaile28@gmail.com>
2022-03-19 11:11:38 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_abs - gives abslote value of an intiger
+ * @int:intiger
+ * Return: Always (success)
+ */
+int _abs(int)
+{
+
+
+	if (int > 0)
+	{
+		_putchar(int);
+	}
+	else if (int < 0)
+	{
+		_putchar(-1 * int);
+	}
+	else
+	{
+		_putchar(0);
+	}
+}
