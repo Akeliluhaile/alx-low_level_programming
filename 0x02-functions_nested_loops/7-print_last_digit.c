67433dda0f6555f81359cac7302ecfe7367f5e32
Akeliluhaile <akeliluhaile28@gmail.com>
2022-03-20 06:54:40 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,11 +2,14 @@
 /**
  * print_last_digit - last digit of a number
  * @ld:last digit of intiger
- * @c:intiger
+ * @c:intige
+ * int: an intiger data type
  * Return:always 0
  */
-int print_last_digit(int c, int ld)
+int print_last_digit(int)
 {
+	int c, ld;
+
 	ld = c % 10;
 
 	_putchar(ld + '0');
