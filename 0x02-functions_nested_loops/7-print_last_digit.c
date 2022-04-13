acd5f5680cce5555dffeca96f86223a03c06fdfb
Akeliluhaile <akeliluhaile28@gmail.com>
2022-04-13 11:58:31 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,15 +2,18 @@
 /**
  * print_last_digit - last digit of a number
  * @ld:last digit of intiger
- * @c:intige
- * int: an intiger data type
- * Return:always 0
+ * @c:intiger
+ * Return:value of last digit of a number
  */
-int print_last_digit(int c, int ld) 
+int print_last_digit(int c)
 {
 
 	ld = c % 10;
 
-	_putchar(ld + '0');
-	return (ld);
+	if (ld < 0)
+	{
+		ld = ld * -1;
+	}
+
+	_putchar(ld + '0')
 }
