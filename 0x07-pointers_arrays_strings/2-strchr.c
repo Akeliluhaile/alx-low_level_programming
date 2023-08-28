18d27617f018da7cd85b2cc3cd8911117b9272a3
Akeliluhaile <Akeliluhaile28@gmail.com>
2023-08-28 06:10:50 -0700

    101-crackme_password

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/**
+/*
  * _strchr - Entry point
  * @s: input
  * @c: input
@@ -8,7 +8,7 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	
+
 
 	for (i = 0; s[i] >= '\0'; i++)
 	{
